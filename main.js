388456aa8efe218d27e1ce53500d3956322968c6
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-01-18 16:49:30 +0100

    Lavoro social posts

--- main.js
+++ main.js
@@ -55,3 +55,60 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+console.log(posts);
+
+const container = document.getElementById('container')
+
+for (let index = 0; index < posts.length; index++) {
+    const element = posts[index];
+
+    container.innerHTML += `<div class="post">
+                                <div class="post__header">
+                                    <div class="post-meta">                    
+                                        <div class="post-meta__icon">
+                                            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                                        </div>
+                                        <div class="post-meta__data">
+                                            <div class="post-meta__author">Phil Mangione</div>
+                                            <div class="post-meta__time">4 mesi fa</div>
+                                        </div>                    
+                                    </div>
+                                </div>
+                                <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.
+                                </div>
+                                    <div class="post__image">
+                                        <img src="https://unsplash.it/600/300?image=171" alt="">
+                                    </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter">
+                                            // Piace a <b id="like-counter-1" class="js-likes-counter"></b> persone
+                                        </div>
+                                    </div> 
+                                </div>`          
+
+}
+let likeButton = document.querySelector('.like-button')
+
+likeButton.addEventListener('click',
+
+    function () {
+
+        console.log('cliccato mi piace')
+
+        let likesCounter = document.querySelector('.likes__counter');
+
+        likesCounter++;
+
+    }      
+    
+)
+
+
